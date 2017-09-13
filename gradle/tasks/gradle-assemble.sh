48fbe9f7e48037842068cb17b0d69277335f13d7
Steve Greenberg <spgreenberg@gmail.com>
2017-09-13 12:55:42 -0600

    gradle clean assemble

--- gradle/tasks/gradle-assemble.sh
+++ gradle/tasks/gradle-assemble.sh
@@ -6,6 +6,6 @@ version=`cat version/number`
 
 cd app
 
-gradle assemble
+gradle clean assemble
 
-mv build/libs/${APP_NAME}*.jar ../build/${JAR_NAME}-${version}.jar
+mv build/libs/*.jar ../build/${JAR_NAME}-${version}.jar
